6a5ca4f24535abedd6ac976d2fe4aa7e60ee25b8
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-05-06 21:47:18 -0400

    IF Else/ Switch Java

---
+++ SwitchJava/src/com/aktar/switching/IfElseClass.java
@@ -0,0 +1,17 @@
+package com.aktar.switching;
+
+public class IfElseClass extends SwitchClass {
+
+	public static void main(String[] args) {
+		Done();
+	int Girls;
+	Girls=20;
+	if (Girls!=20.5){
+		System.out.println("They can go to Party");
+	}else{
+		System.out.println("They have to stay Home");
+	}	
+
+	}
+
+}
---
+++ SwitchJava/src/com/aktar/switching/SwitchClass.java
@@ -0,0 +1,30 @@
+package com.aktar.switching;
+
+public class SwitchClass {
+
+	public static void main(String[] args) {
+	Done();	
+	}
+	public static void Done(){
+		int age;
+		age=4;
+		switch(age){	
+		case 1:
+			System.out.println("Drop in the sea");
+			break;
+		case 2:
+			System.out.println("take in the boat");
+			break;
+		case 3:
+			System.out.println("cook it for Lunch");
+		default:
+			System.out.println("I donot know what to do");
+			break;
+		}			
+}		
+	}		
+		
+		
+
+	
+
